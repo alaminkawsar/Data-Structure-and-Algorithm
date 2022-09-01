ff061d404e72a7a320e71e636024563be155f1a9
alaminkawsar <alaminkawsar8643@gmail.com>
2022-09-01 22:27:34 +0600

    partially matched

--- Data Structure/Segment Tree/Problem/5_LOJ_1125.cpp
+++ Data Structure/Segment Tree/Problem/5_LOJ_1125.cpp
@@ -6,8 +6,9 @@
 #define vi vector<int>
 #define vl vector<long long>
 #define read(a) scanf("%d",&a)
-#define p1(a) cout<<"Check "<<a<<endl;
-#define p2(a,b) cout<<"Check "<<a<<' '<<b<<endl;
+#define p1(a) cout<<"Check1 "<<a<<endl;
+#define p2(a,b) cout<<"Check2 "<<a<<' '<<b<<endl;
+#define p3(a,b, c) cout<<"Check3 "<<a<<' '<<b<<' '<<c<<endl;
 #define fo(i,n) for (i=0;i<n;i++)
 #define ll long long
 #define clr(a,b) memset(a,b,sizeof(a))
@@ -17,7 +18,7 @@ using namespace std;
 int const sz=(int)2e5+5;
 
 struct data {
-    int zero,one,two, prop;
+    int zero=0,one=0,two=0, prop=0;
 };
 
 data tree[sz*4];
@@ -27,7 +28,6 @@ data combine(data left, data right){
     res.one=left.one+right.one;
     res.zero = left.zero+right.zero;
     res.two = left.two+right.two;
-
     return res;
 }
 
@@ -35,7 +35,7 @@ data make_data(int val){
     data res;
     res.one = val;
     res.two=val;
-    res.zero=0;
+    res.zero=val;
     return res;
 }
 
@@ -53,19 +53,32 @@ void build(int v, int tl, int tr) {
     tree[v] = combine(tree[v*2], tree[v*2+1]);
 }
 
-void update(int v, int tl, int tr, int l, int r) {
-    if(l>tr or r<tl) return;
-    if(l<=tl and tr<=r){
-        swap(tree[v].zero,tree[v].two);
-        swap(tree[v].one,tree[v].two);
-        //p2(tl,tr);
-        //p1(tree[v].two);
+void update(int v, int tl, int tr, int l, int r, int carry) {
+
+    if((l<=tl and tr<=r) or (l>tr or r<tl)){
+        if(l<=tl and tr<=r) carry++;
+        int k = carry%3;
+        if(k==1){
+            swap(tree[v].zero,tree[v].one);
+            swap(tree[v].zero,tree[v].two);
+        }else if(k==2){
+            swap(tree[v].zero,tree[v].two);
+            swap(tree[v].zero,tree[v].one);
+        }
+        tree[v].prop=carry;
+        
+        // p3(tl,tr,carry);
+        // p3(tree[v].zero,tree[v].one,tree[v].two);
         return;
     }
     int tm = (tl+tr)/2;
-    update(v*2, tl, tm, l, r);
-    update(v*2+1, tm+1, tr, l, r);
+    int k = tree[v].prop;
+    tree[v].prop=0;
+    
+    update(v*2, tl, tm, l, r, carry+k);
+    update(v*2+1, tm+1, tr, l, r, carry+k);
     tree[v]=combine(tree[v*2],tree[v*2+1]);
+    //printf("Tree %d=(%d,%d)->(%d %d %d)\n",v,tl,tr,tree[v].zero,tree[v].one,tree[v].two);
 }
 
 data getAnswer(int v, int tl, int tr, int l, int r) {
@@ -73,8 +86,6 @@ data getAnswer(int v, int tl, int tr, int l, int r) {
     if(l<=tl and tr<=r){
         return tree[v];
     }
-    if(l==tl && r==tr)
-        return tree[v];
     int tm = (tl+tr)/2;
     return combine(getAnswer(v*2, tl, tm, l, min(r,tm)),
         getAnswer(v*2+1, tm+1, tr, max(l,tm+1), r));
@@ -95,14 +106,13 @@ void solve()
         read(y);
         //action
         if(type==0){
-            update(1,0,n-1,x,y);
+            update(1,0,n-1,x,y,0);
         }
         else{
             printf("%d\n",getAnswer(1,0,n-1,x,y).zero);
         }
-        data ans = getAnswer(1,0,n-1,0,9);
-        printf("HI = %d %d %d\n",ans.zero,ans.one,ans.two);
-        
+        //data ans = getAnswer(1,0,n-1,0,9);
+        //printf("query(%d %d), zero=%d one=%d two=%d\n",x,y,ans.zero,ans.one,ans.two);
     }
     
 }
