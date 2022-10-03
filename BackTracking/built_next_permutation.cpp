f8b7100d2cf2f509de172cb12284b17773116c9d
alaminkawsar <alaminkawsar8643@gmail.com>
2022-10-03 12:16:29 +0600

    built in next permutation

---
+++ BackTracking/built_next_permutation.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#define MAX 100005
+#define pb push_back
+#define mp make_pair
+#define pii pair<long long,long long>
+#define vi vector<int>
+#define vl vector<long long>
+#define read(a) scanf("%d",&a)
+#define p1(a) cout<<"Check "<<a<<endl;
+#define p2(a,b) cout<<"Check "<<a<<' '<<b<<endl;
+#define fo(i,n) for (i=0;i<n;i++)
+#define ll long long
+#define clr(a,b) memset(a,b,sizeof(a))
+#define MOD 1000000007
+
+using namespace std;
+int const sz=(int)2e5+5;
+
+void permute(string str){
+    sort(str.begin(),str.end());
+
+    do {
+        cout<<str<<endl;
+    }while(next_permutation(str.begin(),str.end()));
+}
+
+void solve()
+{
+    string s = "ABCA";
+    permute(s);
+    
+}
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    int t=1,ts=0; cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
