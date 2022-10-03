8c30b9c40cf00b3d3723cb702e5cdfa74989856c
alaminkawsar <alaminkawsar8643@gmail.com>
2022-10-03 12:08:14 +0600

    generate permutation for a given string

---
+++ BackTracking/print_permuation.cpp
@@ -0,0 +1,49 @@
+//print all the permutation for a given string
+
+#include<bits/stdc++.h>
+#define MAX 100005
+#define pb push_back
+#define mp make_pair
+#define pii pair<long long,long long>
+#define vi vector<int>
+#define vl vector<long long>
+#define read(a) scanf("%d",&a)
+#define p1(a) cout<<"Check "<<a<<endl;
+#define p2(a,b) cout<<"Check "<<a<<' '<<b<<endl;
+#define fo(i,n) for (i=0;i<n;i++)
+#define ll long long
+#define clr(a,b) memset(a,b,sizeof(a))
+#define MOD 1000000007
+
+using namespace std;
+int const sz=(int)2e5+5;
+
+void printPermuation(string s,int l, int r){
+    if(l>=r){
+        cout<<s<<endl;
+        return;
+    }
+    for(int i=l;i<=r;i++){
+        swap(s[i],s[l]);
+        printPermuation(s,l+1,r);
+        swap(s[i],s[l]);
+    }
+}
+
+void solve()
+{
+    string  str = "ABCD";
+    printPermuation(str,0,str.size()-1);
+
+    
+}
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    //freopen("output.txt","w",stdout);
+    int t=1,ts=0; //cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
