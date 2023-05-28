f9b555fb40cedeeb5df540087cdbd590421328ef
alaminkawsar <alaminkawsar8643@gmail.com>
2023-05-29 05:45:13 +0600

    making union find structure

--- Data Structure/Union-Finding(Disjoint-Set)/1_disjoint_set.cpp
+++ Data Structure/Union-Finding(Disjoint-Set)/1_disjoint_set.cpp
@@ -5,40 +5,33 @@ using namespace std;
 typedef long long ll;
 const int sz = 1e5+10;
 
-vector<int>adj[sz];
-int parent[sz+5],ran[sz+5],id[sz+5];
-
-void make_set(int v) {
-    parent[v] = v;
-}
-
-int find_set(int v) {
-    if (v == parent[v])
-        return v;
-    return parent[v] = find_set(parent[v]);
-}
-
-void union_sets(int a, int b) {
-    a = find_set(a);
-    b = find_set(b);
-    if (a != b)
-        parent[b] = a;
-}
+struct UnionFind {
+    std::vector<int> parent;
+    UnionFind(int n): parent(n+5){
+        for(int i=1;i<=n;i++) parent[i]=i;
+    }
+    int find(int v) {
+        if (v == parent[v]) return v;
+        return parent[v] = find(parent[v]);
+    }
+    void merge(int a, int b) {
+        a = find(a),b = find(b);
+        if (a != b) parent[b] = a;
+    }
+};
 
 int main(){
     int n = 6, edge=5; 
-    
-    for(int i=1;i<=n;i++) make_set(i);
-    
+    UnionFind u(n);    
     int x,y;
     for(int i=0;i<edge;i++){
         cin>>x>>y;
-        union_sets(x,y);
+        u.merge(x,y);
     }
     vector<bool>vis(n+5,false);
     int cnt = 0;
     for(int i=1;i<=n;i++){
-        x = find_set(i);
+        x = u.find(i);
         if(vis[x]) continue;
         vis[x]=true;
         cnt++;
